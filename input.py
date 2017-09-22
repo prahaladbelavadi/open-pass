64fd145a75e3d3879f95a43bbd3035d6a3e6da55
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-09-22 16:22:41 +0530

    take input and split into two files

--- input.py
+++ input.py
@@ -1,5 +1,23 @@
+import json
+
 client = raw_input("Enter client name:")
 url = raw_input("Enter web URL:")
 meta = raw_input("Enter any meta data:")
 
-print client, url, meta
+data = {
+'name': client,
+'url':url,
+'meta':meta
+}
+
+print data
+
+with open('data.json', 'w') as f:
+     json.dump(data, f)
+
+file_state = open("filename.txt", "a")
+file_state.write('\n'+client+'\n')
+file_state.write('\n'+url+'\n')
+file_state.write('\n'+meta+'\n')
+
+file_state.close
