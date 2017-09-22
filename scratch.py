ece656163d56050a1e6bda27ab1bfa92379fac00
Prahalad Belavadi <prahaladbelavadi@gmail.com>
2017-09-22 16:23:57 +0530

    empty gitignore

---
+++ scratch.py
@@ -0,0 +1,24 @@
+# filename = input ("filename: ");
+# with open (filename, "w") as f:
+#   f.write (input ());
+
+# file-output.py
+# f = open('helloworld.txt','w')
+# f.write('hello world')
+# f.close()
+
+
+# file-append.py
+# f = open('helloworld.txt','a')
+# f.write('\n' + 'hello world')
+# f.close()
+
+
+#
+# file_state = open("filename.txt", "a")
+# # file_state.write([client,url,meta])
+# json.dump(client, url,meta)
+# file_state.close
+
+# with open('data.json', 'w') as f:
+#      json.dump(data, f)
